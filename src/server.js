9b9280cadf65ee96c5a1d712c221df7c2b349ded
Elizeurjr <elizeuribeiroo@gmail.com>
2023-01-27 21:49:30 -0300

    desafio 2

--- src/server.js
+++ src/server.js
@@ -50,6 +50,6 @@ app.get('/', async (req, res) => {
 });
 
 models.initDatabase();
-app.listen(8080);
+app.listen(8000);
 
-console.log('Aplicação rodando na porta 8080');
+console.log('Aplicação rodando na porta 8000');
